5439d780071a3cf1205fed009e8bb390d4dac851
dtruong <dtruong3@yahoo.com>
2019-07-27 12:00:27 -0400

    new setup of Nserver

---
+++ NServer/src/main/java/com/nordic/Nserver/NserverApplication.java
@@ -0,0 +1,13 @@
+package com.nordic.Nserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NserverApplication.class, args);
+	}
+
+}
